afbde2868ee3c6b5eb6662545e02fb6bf0ed1e06
m1crovolnovka <denisdevyaltovski@gmail.com>
2026-05-21 14:55:29 +0300

    fix: frontend

--- frontend/user-app/vite.config.ts
+++ frontend/user-app/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://p01--telegramminiapp--jrk5phf8xmp4.code.run',
         changeOrigin: true,
       },
     },
